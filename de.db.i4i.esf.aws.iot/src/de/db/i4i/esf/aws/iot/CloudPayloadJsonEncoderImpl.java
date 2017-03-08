dc2bba192daa1c2aa58272f46ce1df3c5dda5947
bruno <bruno@DESKTOP-J3NJPPO>
2017-03-08 22:47:17 +0100

    Backup

--- de.db.i4i.esf.aws.iot/src/de/db/i4i/esf/aws/iot/CloudPayloadJsonEncoderImpl.java
+++ de.db.i4i.esf.aws.iot/src/de/db/i4i/esf/aws/iot/CloudPayloadJsonEncoderImpl.java
@@ -4,18 +4,20 @@ import java.io.IOException;
 
 import org.eclipse.kura.message.KuraPayload;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class CloudPayloadJsonEncoderImpl implements CloudPayloadEncoder {
 
-	private final KuraPayload kuraPayload;
+	private final KuraPayload2Json payload;
 
     public CloudPayloadJsonEncoderImpl(KuraPayload kuraPayload) {
-        this.kuraPayload = kuraPayload;
+        this.payload = new KuraPayload2Json(kuraPayload);
     }
 	
 	@Override
 	public byte[] getBytes() throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		ObjectMapper mapper = new ObjectMapper();
+		return mapper.writeValueAsBytes(payload);
 	}
 
 }
---
+++ de.db.i4i.esf.aws.iot/src/de/db/i4i/esf/aws/iot/KuraPayload2Json.java
@@ -0,0 +1,27 @@
+package de.db.i4i.esf.aws.iot;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.eclipse.kura.message.KuraPayload;
+import org.eclipse.kura.message.KuraPosition;
+
+public class KuraPayload2Json {
+	public Date sentOn;
+	public KuraPosition position;
+	public Map<String, Object> metrics;
+	public byte[] body;
+	public KuraPayload2Json(KuraPayload payload) {
+		this.sentOn = payload.getTimestamp();
+		this.position = payload.getPosition();
+		this.metrics = new HashMap<String, Object>();
+		String key;
+		for ( Iterator<String> it = payload.metricsIterator(); it.hasNext(); ) {
+			key = it.next();
+			this.metrics.put(key, payload.getMetric(key));
+		}
+		this.body = payload.getBody();
+	}
+}
